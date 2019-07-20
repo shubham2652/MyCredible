e8efe4869bd6d0e5d88c687f078ec9e26b9989a2
Shubham Shah <shahshubham60@gmail.com>
2019-07-20 21:59:15 +0530

    Initial commit

---
+++ app/src/main/java/com/sparksfoundation/mycredible/PersonalDetailsPOJOClasses/PersonalDetailsData.java
@@ -0,0 +1,25 @@
+package com.sparksfoundation.mycredible.PersonalDetailsPOJOClasses;
+
+public class PersonalDetailsData
+{
+    private Data data;
+
+    public PersonalDetailsData() {
+    }
+
+    public Data getData ()
+    {
+        return data;
+    }
+
+    public void setData (Data data)
+    {
+        this.data = data;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "ClassPojo [data = "+data+"]";
+    }
+}
